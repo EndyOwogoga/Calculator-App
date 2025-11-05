8e3ef9968bd6c5f9c319fccbd52f86b65f80222f
Onah Endurance Owogoga <125702519+EndyOwogoga@users.noreply.github.com>
2025-11-05 10:13:30 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,24 @@
+//CALCULATOR PROGRAM
+
+const display = document.querySelector(".display");
+
+function appendToDisplay(input){
+    display.value += input;
+}
+
+function clearDisplay() {
+    display.value = "";
+}
+
+function deleteChar() {
+  display.value = display.value.slice(0, -1);
+}
+
+function calculate() {
+    try{
+        display.value = eval(display.value);
+    }
+    catch(error){
+        display.value = "Syntax Error";
+    }
+}
